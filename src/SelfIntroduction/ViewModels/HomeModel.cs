bcecbc6a93f86ef5230e8895e9abe80b14e2f4ae
WillianHsiao <lucien5335@hotmail.com>
2016-06-16 22:16:04 +0800

    Add HomeModel

---
+++ src/SelfIntroduction/ViewModels/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfIntroduction.ViewModels
+{
+    public class HomeModel
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
